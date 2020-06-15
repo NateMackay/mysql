7a5ea57dd19bd4b7fa917fd4fc72610e91e120cc
Nate Mackay <mac17039@byui.edu>
2020-06-15 13:25:02 -0600

    add files to access API

---
+++ db.sql
@@ -0,0 +1,9 @@
+
+
+-- create a table for the meal
+CREATE TABLE food (id INT PRIMARY KEY, name VARCHAR(255), youtubeLink VARCHAR(255), picture VARCHAR(255));
+
+-- create a table for the ingredients
+CREATE TABLE ingreds (id INT, portions VARCHAR(255), ingredient VARCHAR(255));
+
+
